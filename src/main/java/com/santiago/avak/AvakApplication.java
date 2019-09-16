bfee2bd697ea04cd1d1f5614e9b35abc69b0edc3
Thiago Santiago <thiago.vps20@gmail.com>
2019-09-16 09:38:11 -0300

    Projeito inicial

---
+++ src/main/java/com/santiago/avak/AvakApplication.java
@@ -0,0 +1,13 @@
+package com.santiago.avak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvakApplication.class, args);
+	}
+
+}
