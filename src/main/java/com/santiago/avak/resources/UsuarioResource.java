7cb83b946c52dc6c6f03b624571d7936187544a6
Thiago Santiago <thiago.vps20@gmail.com>
2019-09-22 13:25:38 -0300

    Add pre autorização nas rotas

--- src/main/java/com/santiago/avak/resources/UsuarioResource.java
+++ src/main/java/com/santiago/avak/resources/UsuarioResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -68,6 +69,7 @@ public class UsuarioResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		this.service.delete(id);
