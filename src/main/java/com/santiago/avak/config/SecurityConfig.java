d26153d3d26d8e450d1f18bb115971bb1d82e1bb
Thiago Santiago <thiago.vps20@gmail.com>
2019-09-22 19:16:49 -0300

    Configurando cors

--- src/main/java/com/santiago/avak/config/SecurityConfig.java
+++ src/main/java/com/santiago/avak/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 	private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
 	private static final String[] PUBLIC_MATCHERS_GET = {"/cursos/**"};
-	private static final String[] PUBLIC_MATCHERS_POST = {"/usuarios/**"};
+	private static final String[] PUBLIC_MATCHERS_POST = {"/usuarios/**", "/auth/forgot/**"};
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -50,7 +50,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers( "/auth/forgot/**").permitAll()
 		.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
@@ -68,8 +67,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// Liberando o cors com configurações default
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
 	
